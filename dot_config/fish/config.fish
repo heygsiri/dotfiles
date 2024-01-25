00a12c7e830394b66fe16e22c87bb64f29045900
Gabriel Sirilan <69453260+heygsiri@users.noreply.github.com>
2024-01-25 01:44:07 +0000

    feat: add transience to fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,6 +1,11 @@
+function starship_transient_prompt_func
+  starship module character
+end
+
 if status --is-interactive
     # obtain script directory
     set DIR (dirname (status --current-filename))
     source $DIR/zellij_completions.fish
     starship init fish | source
+    enable_transience
 end
